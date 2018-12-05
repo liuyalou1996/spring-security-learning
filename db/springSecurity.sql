f92d97aba7cac89d54c20e94606984be25d52bf8
liuyalou <liuyalou@iboxpay.com>
2018-12-05 21:00:07 +0800

    sql脚本更改

--- db/springSecurity.sql
+++ db/springSecurity.sql
@@ -1,33 +1,68 @@
-create table users(
-	username varchar(50) not null primary key,
-	password varchar(100) not null,
-	enabled boolean not null
-);
+/*
+ Navicat Premium Data Transfer
 
-create table authorities (
-	username varchar(50) not null,
-	authority varchar(50) not null,
-	constraint fk_authorities_users foreign key(username) references users(username)
-);
+ Source Server         : localhost_3306
+ Source Server Type    : MySQL
+ Source Server Version : 50173
+ Source Host           : localhost:3306
+ Source Schema         : springsecurity
 
-create unique index ix_auth_username on authorities (username,authority);
+ Target Server Type    : MySQL
+ Target Server Version : 50173
+ File Encoding         : 65001
 
-create table persistent_logins (
-	username varchar(64) not null,
-	series varchar(64) primary key,
-	token varchar(64) not null,
-	last_used timestamp not null
-);
+ Date: 29/11/2018 15:37:58
+*/
 
-insert into users(username,password,enabled) values('lq','123',1);
-insert into authorities(username,authority) values('lq','ROLE_USER');
-update users set password='{bcrypt}$2a$10$VmYUmu3CnAjm28gCmHCHAu3SMcEbeOIqXqfu7y3OUJT2VlVznXHPe';
+SET NAMES utf8;
+SET FOREIGN_KEY_CHECKS = 0;
 
-select * from users;
-select * from authorities;
-select * from persistent_logins;
+-- ----------------------------
+-- Table structure for authorities
+-- ----------------------------
+DROP TABLE IF EXISTS `authorities`;
+CREATE TABLE `authorities`  (
+  `username` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `authority` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  UNIQUE INDEX `ix_auth_username`(`username`, `authority`) USING BTREE,
+  CONSTRAINT `fk_authorities_users` FOREIGN KEY (`username`) REFERENCES `users` (`username`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
-delete from users where username='zxq';
-delete from authorities where username='zxq';
-show tables;
-describe users;
+-- ----------------------------
+-- Records of authorities
+-- ----------------------------
+INSERT INTO `authorities` VALUES ('lq', 'ROLE_USER');
+INSERT INTO `authorities` VALUES ('lyl', 'ROLE_USER');
+INSERT INTO `authorities` VALUES ('zxq', 'ROLE_USER');
+
+-- ----------------------------
+-- Table structure for persistent_logins
+-- ----------------------------
+DROP TABLE IF EXISTS `persistent_logins`;
+CREATE TABLE `persistent_logins`  (
+  `username` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `series` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `token` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `last_used` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`series`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users`  (
+  `username` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `password` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `enabled` tinyint(1) NOT NULL,
+  PRIMARY KEY (`username`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of users
+-- ----------------------------
+INSERT INTO `users` VALUES ('lq', '{bcrypt}$2a$10$VmYUmu3CnAjm28gCmHCHAu3SMcEbeOIqXqfu7y3OUJT2VlVznXHPe', 1);
+INSERT INTO `users` VALUES ('lyl', '{bcrypt}$2a$10$VmYUmu3CnAjm28gCmHCHAu3SMcEbeOIqXqfu7y3OUJT2VlVznXHPe', 1);
+INSERT INTO `users` VALUES ('zxq', '{bcrypt}$2a$10$VmYUmu3CnAjm28gCmHCHAu3SMcEbeOIqXqfu7y3OUJT2VlVznXHPe', 1);
+
+SET FOREIGN_KEY_CHECKS = 1;
